9946b3dccd30f989f4e154bbb1135846628be03e
Vinícius Chaves <106564445+vpireschaves@users.noreply.github.com>
2024-04-13 15:19:22 -0300

    feat: index para visualização do chatbot ao acessar o servidor

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static('./publico'));
+app.use(express.static('./public'));
 
 app.use('/dialogflow', dialogFlowRoute);
 
